7476416dc2611de96e2197320ca1042d85abb40a
Germán Fernández <german.frz@gmail.com>
2021-12-01 18:44:48 -0300

    Agregué la definición de un helper para Handlebars (truncate)

--- backend/app.js
+++ backend/app.js
@@ -73,5 +73,19 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
+// helpers
+
+Handlebars.registerHelper ('truncate', function (str, len) {
+    if (str.length > len && str.length > 0) {
+        var new_str = str + " ";
+        new_str = str.substr (0, len);
+        new_str = str.substr (0, new_str.lastIndexOf(" "));
+        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+        return new Handlebars.SafeString ( new_str +'...' ); 
+    }
+    return str;
+});
+
 
 module.exports = app;
